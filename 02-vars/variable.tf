d763ad166db5a5768f5fd49d577a3c5e32fe5df9
Ravi <raviteja121683@gmail.com>
2023-09-16 22:45:39 +0530

    Autopush

--- 02-vars/variable.tf
+++ 02-vars/variable.tf
@@ -1,7 +1,11 @@
+#declaring a variable
 variable "sample" {
   default = "hello ravi"
 }
-
+#printing a variable
 output "op" {
    value = var.sample
+}
+output "ops" {
+  value = "my first terraform message is ${var.sample}"
 }
