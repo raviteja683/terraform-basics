badb9b09f7abe47c6249d6edff7a8eacb4302f8e
Ravi <raviteja121683@gmail.com>
2023-09-20 01:17:58 +0530

    Autopush

--- 03-sample-vm/vm.tf
+++ 03-sample-vm/vm.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "web" {
   ami           = "ami-0c1d144c8fdd8d690"
-  instance_type = "t3.micro"
+  instance_type = "t3.medium"
 
   tags = {
     Name = "Terrafrom-instance"
