6e6cbb6cc187592c8983f7374f442ae02c4435b2
Ravi <raviteja121683@gmail.com>
2023-09-29 15:17:05 +0530

    Autopush

--- 04-remote-state/main.tf
+++ 04-remote-state/main.tf
@@ -1,8 +1,9 @@
 resource "aws_instance" "web" {
   ami           = "ami-0c1d144c8fdd8d690"
   instance_type = "t3.micro"
+  count  = 2
 
   tags = {
-    Name = "Terrafrom-web-instance"
+    Name = "Terrafrom-web-instance-${count.index}"
   }
 }
