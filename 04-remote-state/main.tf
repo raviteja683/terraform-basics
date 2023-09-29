0a9ac00d9331ab94857cfa28d7d2767ec0509dba
Ravi <raviteja121683@gmail.com>
2023-09-29 17:17:45 +0530

    Autopush

--- 04-remote-state/main.tf
+++ 04-remote-state/main.tf
@@ -1,6 +1,6 @@
 data "aws_ami" "ami" {
   most_recent      = true
-  name_regex       = "DevOps-LabImage-CentOS7"
+  name_regex       = "TESTING-AMI"
   owners           = ["self"]
 }
 resource "aws_instance" "web" {
