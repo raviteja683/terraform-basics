e6e5e3c3c13f5a6c5583b60e083da18c92bcdc86
Ravi <raviteja121683@gmail.com>
2023-09-29 18:37:12 +0530

    Autopush

--- 03-sample-vm/vm.tf
+++ 03-sample-vm/vm.tf
@@ -6,3 +6,34 @@ resource "aws_instance" "web" {
     Name = "Terrafrom-instance"
   }
 }
+
+output "private_IP_Address" {
+  value = aws_instance.web.private_dns
+}
+
+#create a security group
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+  //vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description      = "SSH from Internet"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
