d923ede0e47ed3b40fc0601c48ba6d6e53ba9741
Ravi <raviteja121683@gmail.com>
2023-09-29 18:46:51 +0530

    Autopush

--- 03-sample-vm/vm.tf
+++ 03-sample-vm/vm.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "web" {
-  ami           = "ami-0c1d144c8fdd8d690"
-  instance_type = "t3.medium"
+  ami                    = "ami-0c1d144c8fdd8d690"
+  instance_type          = "t3.medium"
+  vpc_security_group_ids = ["aws_security_group.allow_ssh.id"]
 
   tags = {
     Name = "Terrafrom-instance"
