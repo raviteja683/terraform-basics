c076925693813136a83364bd2669afbb65aa04f4
Ravi <raviteja121683@gmail.com>
2023-09-29 16:14:20 +0530

    Autopush

--- 04-remote-state/main.tf
+++ 04-remote-state/main.tf
@@ -1,5 +1,12 @@
+data "aws_ami" "ami" {
+  //executable_users = ["self"]
+  most_recent      = true
+  name_regex       = "DevOps-LabImage-CentOS7"
+  owners           = ["self"]
+}
 resource "aws_instance" "web" {
-  ami           = "ami-0c1d144c8fdd8d690"
+  //ami           = "ami-0c1d144c8fdd8d690"
+  ami           = "data.aws_ami.ami.image_id"
   instance_type = "t3.micro"
   count  = var.howManyYouwant
 
@@ -8,5 +15,6 @@ resource "aws_instance" "web" {
   }
 }
 variable "howManyYouwant" {
-   default = 2
+   default = 1
 }
+
